280fdc55bdefd09aaa0421b51aa7552d4d464d26
ziraq young <ziraqyoung@outlook.com>
2021-10-03 00:36:18 +0300

    Add extra plugins for tailwindcss
    - Add official tailwind plugins: forms,
    aspect-ratio, line-clamp and typography
    - Stub default tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ]
 }
