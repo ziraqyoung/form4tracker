c504bed88de6477fc1ca2707df7139df2cb6279d
ziraq young <ziraqyoung@outlook.com>
2021-10-04 23:32:50 +0300

    Grab all form values from mortgage_controller

--- app/javascript/controllers/mortgage_controller.js
+++ app/javascript/controllers/mortgage_controller.js
@@ -1,7 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log('Hello from  mortgage controller')
+  static targets = [ "homePrice", "downPayment", "downPercent", "interestRate"]
+
+  calculate(event) {
+    event.preventDefault();
+  }
+
+  // getter functions
+  get home_price() {
+    return this.homePriceTarget.value
+  }
+
+  get down_payment() {
+    return this.downPaymentTarget.value
+  }
+
+  get down_percent() {
+    return this.downPercentTarget.value
+  }
+
+  get interest_rate() {
+    return this.interestRateTarget.value
   }
 } 
