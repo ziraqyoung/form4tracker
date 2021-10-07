6dfb25d401e597cc649c15acf2f7b9c4b91a2c4c
ziraq young <ziraqyoung@outlook.com>
2021-10-07 21:22:51 +0300

    Calculate Monthly mortgage payment
    - Add a field for mortgage term
    - Populate mortgage_controller with logic to calculate monthly mortgage

--- app/javascript/controllers/mortgage_controller.js
+++ app/javascript/controllers/mortgage_controller.js
@@ -1,27 +1,62 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "homePrice", "downPayment", "downPercent", "interestRate"]
+  static targets = [ "homePrice", "downPayment", "downPercent", "interestRate", "mortgageTerm"]
 
   calculate(event) {
     event.preventDefault();
-    console.log("Mortgager calculate montly payment")
+
+    console.log("Printing before logging")
+
+    console.log(`ComputedAmount: ${this.computedAmount()}`) 
+    console.log(`ComputedRate: ${this.computedRate()}`) 
+    console.log(`ComputedTerm: ${this.computedTerm()}`) 
+
+    console.log(`getMontlyPayment: ${this.getMontlyPayment()}`) 
+
+    this.alertResults()
   }
 
-  // getter functions
+  // display helper functions
+  alertResults() {
+    window.alert(`The calculated Mortgage Montly Payment: $${this.getMontlyPayment()}`);
+  }
+
+  // calculator helper functions
+  getMontlyPayment() {
+    return this.computedAmount() * ( this.computedRate() * Math.pow(this.computedRate() + 1, this.computedTerm())) / ( Math.pow(this.computedRate() + 1, this.computedTerm()) -1 );
+  }
+
+  computedAmount() {
+    return this.home_price - this.down_payment
+  }
+
+  computedRate() {
+    return ((this.interest_rate / 100 ) / 12)
+  }
+
+  computedTerm() {
+    return (this.mortgage_term * 12)
+  }
+
+  // input getter functions
   get home_price() {
-    return this.homePriceTarget.value
+    return parseInt(this.homePriceTarget.value)
   }
 
   get down_payment() {
-    return this.downPaymentTarget.value
+    return parseInt(this.downPaymentTarget.value)
   }
 
   get down_percent() {
-    return this.downPercentTarget.value
+    return parseInt(this.downPercentTarget.value)
   }
 
   get interest_rate() {
-    return this.interestRateTarget.value
+    return parseInt(this.interestRateTarget.value)
+  }
+
+  get mortgage_term() {
+    return parseInt(this.mortgageTermTarget.value)
   }
 } 
