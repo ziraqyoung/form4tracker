a5fd71f795fa87bc6a79052d43b5537388c9c5c7
ziraq young <ziraqyoung@outlook.com>
2021-10-10 03:17:03 +0300

    Remove default hello_controller & update gems

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-import MortgageController from "./mortgage_controller"
+import MortgageController from "./mortgage_controller.js"
 application.register("mortgage", MortgageController)
