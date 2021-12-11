2ccd8fd700121c4ed4c3f292c196f63b8f4cb5a8
ziraq young <ziraqyoung@outlook.com>
2021-12-11 23:21:22 +0300

    Store Company information
    - Generate Company model
    - Add colors to tailwind default configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
   root 'landing#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  resources :charts, only: :index
+  resources :companies, only: [:index, :show]
+
+  # api/prices?company_id=1
+  namespace :api do
+    resources :prices
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
